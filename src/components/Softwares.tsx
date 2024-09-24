e5c0a36b09a2cd7863c1b84f83d78ca5e03900da
melsonic <madhurjyakalita2017@gmail.com>
2024-09-24 23:06:45 +0530

    updated softwares styling

--- src/components/Softwares.tsx
+++ src/components/Softwares.tsx
@@ -13,16 +13,19 @@ function Softwares() {
     <div className="pitembox" id="software">
       <h1 className="psection-title">Software</h1>
       {/* <div className="flex flex-wrap justify-center"> */}
-      <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-y-4 lg:gap-x-8 2xl:gap-x-16">
+      <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-y-8 lg:gap-x-16 2xl:gap-x-32">
         {softwareList.map((i: Software, idx: number) => {
           const { title, description, demoImage, livePreview, githubRepo } = i;
+          let derivedDescription = description.length > 80
+            ? description.slice(0, 80) + "..."
+            : description;
           const softwareBoxAllignSelf: string = idx % 2 === 0
             ? "lg:justify-self-end"
             : "lg:justify-self-start";
           const softwareClasses: string =
-            `flex flex-col pgradient-border w-88 my-4 p-8 box-border justify-self-center sm:w-144 md:h-fit lg:w-96 lg:h-136 2xl:w-112 ${softwareBoxAllignSelf}`;
+            `flex flex-col pgradient-border w-88 my-4 p-8 box-border justify-self-center sm:w-144 md:h-fit lg:w-96 2xl:w-112 ${softwareBoxAllignSelf}`;
           return (
-            <div className={softwareClasses} key={idx} >
+            <div className={softwareClasses} key={idx}>
               <img
                 src={getImageUrl(demoImage).url()}
                 alt="software image"
@@ -31,7 +34,9 @@ function Softwares() {
               <span className="text-2xl font-bold pgradient-text mt-8 mb-4">
                 {title}
               </span>
-              <span className="flex-1 sm:mb-20 lg:mb-0">{description}</span>
+              <span className="flex-1">
+                <abbr title={description}>{derivedDescription}</abbr>
+              </span>
               <span className="grid grid-cols-2">
                 <button className="pbutton-gradient-mobile">
                   <a href={livePreview} target="_blank">Live Preview</a>
