57f6135c2c838d36524d83bcedd812ccc2748f5f
melsonic <madhurjyakalita2017@gmail.com>
2025-01-13 00:45:26 +0530

    Revert "some code clean up"
    This reverts commit bfd2a93d434567c8719c9664bf43d3dde0749707.

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -12,3 +12,15 @@ export async function getExperienceList(): Promise<Experience[]> {
   return experiences;
 }
 
+// export const experiences: Array<Experience> = [
+//   {
+//     title: "PROJECT INTERN IN NETSUITE GBU, ORACLE",
+//     timeline: "MAY-JULY, 2023",
+//     highlights: [
+//       "Worked on developing a feature in NetSuite for automating inventory level management, empowering business owners to prevent stockouts proactively.",
+//       "Utilized SuiteScript API for extending the NetSuite functionality and seamlessly integrating the solution to the software.",
+//       "During the internship, I gained hands-on experience in the complete Software Development Life Cycle (SDLC),honing skills in Software Testing and SuiteScript API.",
+//     ],
+//     certificate: DocumentsList.oracleIntern,
+//   },
+// ];
